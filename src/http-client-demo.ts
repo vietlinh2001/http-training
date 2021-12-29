4992baec7123bd1b2bb3a7a453989f55771aa33a
Viet Linh <vietlinhdt2001@gmail.com>
2021-12-29 15:25:35 +0700

    http

--- src/http-client-demo.ts
+++ src/http-client-demo.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 
-const userUrl = `https://jsonplaceholder.typicode.com/users/${process.argv[2]}`
-
+//const userUrl = `https://jsonplaceholder.typicode.com/users/${process.argv[2]}`
+const userUrl=`http://localhost:3000/`
 axios.get(userUrl).then(({ data: user }) => {
     console.log(`Hello ${user.name}`)
 })
+
+
+// axios.get(userUrl).then((res)=>{
+//     const data=res.data
+//     const user=data
+//     console.log(user.name)
+
+// })
+
+
+
